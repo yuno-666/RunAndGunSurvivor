4bb6f7d415701e237730bf10bf204ebb45604def
K_H\f20da <214336728+yuno- 666@users.noreply.github.com>
2026-03-03 15:07:46 +0900

    Shooterの実装

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -18,38 +18,89 @@ public class BulletManager : MonoBehaviour
     //弾の消費
     public void ConsumeBullet()
     {
-       
+        if (bulletRemaining > 0)//残弾があれば
+        {
+            bulletRemaining--;//一つ減らす
+        }
+
     }
 
     //残数の取得
     public int GetBulletRemaining()
     {
-        return 10;
+        return bulletRemaining;
     }
 
     //弾の充填
     public void AddBullet(int num)
     {
-       
+        //今の残数を決められた最大の数にする
+        bulletRemaining = num;//残弾数を最大にする
     }
 
     //充填メソッド
     public void RecoverBullet()
     {
-       
+        if (bulletRecover == null)//コルーチンが発動していない
+        {
+            if (magazine > 0)//マガジンの残数があれば
+            {
+                magazine--;//マガジンを消費
+
+                //コルーチンを発動とコルーチン情報を変数に格納
+                bulletRecover = StartCoroutine(RecoverBulletCol());
+
+            }
+        }
     }
 
     //充填コルーチン
     IEnumerator RecoverBulletCol()
     {
-       
-        yield return new WaitForSeconds(1.0f); //ウェイト処理
-         
+        counter = recoveryTime;//グローバル変数counterのセットアップ
+        while (counter>0)
+        {
+            yield return new WaitForSeconds(1.0f);//ウェイト処理
+            counter--;
+        }
+        //Debug.Log("コルーチンスタート！");
+        //何かしらの処理
+        //yield return new WaitForSeconds(recoveryTime); //ウェイト処理
+        AddBullet(maxRemaining);
+        bulletRecover = null;//コルーチン情報をリセット
     }
 
     //画面上に簡易GUI表示
     void OnGUI()
     {
+        //残弾数を表示（左50、上50、幅100、高さ30：黒）
+        GUI.color = Color.black;
+        string label = "Bullet: " + bulletRemaining;
+        GUI.Label(new Rect(50, 50, 100, 30), label);
+
+        //残マガジンを表示（左50、上75、幅100、高さ30：黒)
+        label = "magazine: " + magazine;
+        GUI.Label(new Rect(50, 75, 100, 30), label);
+
+        //充填開始～充填完了まで（左50、上25、幅100、高さ30：赤)
+        //赤文字点滅
+        
+        if (bulletRecover != null)
+        {
+            GUI.color = Color.red;//赤字
+
+            float val = Mathf.Sin(Time.time * 50);
+            if (val > 0)
+            {
+                label = "bulletRecover：" + counter;
+            }
+            GUI.Label(new Rect(50, 25, 100, 30), label);
+            //else
+            //{
+            //    label = "";
+            //}
+
+        }
        
     }
 }
--- Assets/Scripts/NormalShooter.cs
+++ Assets/Scripts/NormalShooter.cs
@@ -15,7 +15,7 @@ public class NormalShooter : MonoBehaviour
     public float shootSpeed = 10.0f; //弾速
 
     GameObject bullets; //生成した弾をまとめるオブジェクト
-    
+
     //InputAction(Playerマップ)のAttackアクションがおされたら
     void OnAttack(InputValue value)
     {
@@ -24,11 +24,32 @@ public class NormalShooter : MonoBehaviour
 
     void Shoot()
     {
-       
+        //残弾があれば
+        if (bulletManager.GetBulletRemaining() > 0)
+        {
+            //プレハブの生成と生成情報の取得
+            GameObject obj = Instantiate(
+                bulletPrefabs,//何を
+                gate.transform.position,//どこに
+                Quaternion.Euler(90, 0, 0)//どの角度で
+                );
+
+            //弾の消費
+            bulletManager.ConsumeBullet();
+            //生成したbullet自身のRigidbodyの力で飛ばす
+            Rigidbody bulletRbody = obj.GetComponent<Rigidbody>();
+            bulletRbody.AddForce(new Vector3(0, 0, shootSpeed), ForceMode.Impulse);
+        }
+        else
+        {
+            //残弾がなければマガジンを消費して充填開始
+            bulletManager.RecoverBullet();
+        }
+
     }
 
     void Start()
     {
-        
-    }    
+
+    }
 }
